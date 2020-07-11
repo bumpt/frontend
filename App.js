89c49f052bbaed6f7f96ac344f5fdee06a960448
Miguel Nicolas <miguelqnicolas@gmail.com>
2020-07-11 16:20:36 -0400

    Lay out basic landing page

--- App.js
+++ App.js
@@ -7,108 +7,61 @@
  */
 
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity, Alert } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+// const App: () => React$Node = () => {
+const App = () => {
+	const onPress = () => {
+		Alert.alert('Test');
+	};
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
+	return (
+		<View style={styles.container}>
+			<Text style={styles.h1}>Bumpt</Text>
+			<Text style={styles.h2}>Virtual business cards</Text>
+			<TouchableOpacity style={styles.button} onPress={onPress}>
+				<Text style={styles.buttonText}>Sign Up with Google</Text>
+			</TouchableOpacity>
+			<TouchableOpacity style={styles.button} onPress={onPress}>
+				<Text style={styles.buttonText}>Sign Up with Facebook</Text>
+			</TouchableOpacity>
+			<TouchableOpacity style={styles.button} onPress={onPress}>
+				<Text style={styles.buttonText}>Sign Up with Email</Text>
+			</TouchableOpacity>
+			<Text style={styles.p}>By signing up, you agree to our Terms of Service</Text>
+			<Text style={styles.p}>and Privacy Policy.</Text>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	h1: {
+		fontSize: 32
+	},
+	h2: {
+		marginBottom: 32,
+		fontSize: 16
+	},
+	p: {
+		fontSize: 12
+	},
+	button: {
+		width: '90%',
+		marginBottom: 16,
+		padding: 12,
+		borderRadius: 4,
+		backgroundColor: '#ffffff'
+	},
+	buttonText: {
+		fontSize: 16,
+		color: '#333',
+		textAlign: 'center'
+	}
 });
 
 export default App;
