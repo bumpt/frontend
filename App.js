c31be3b133a644e238c7d47f8991f0cec38a65d1
Miguel Nicolas <miguelqnicolas@gmail.com>
2020-07-27 13:51:57 -0400

    Test touchable opacity

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React from 'react';
-import { StyleSheet, View, Text, TouchableOpacity, Alert } from 'react-native';
+import { StyleSheet, SafeAreaView, View, Text, TouchableOpacity, Alert } from 'react-native';
 
 // const App: () => React$Node = () => {
 const App = () => {
@@ -16,9 +16,16 @@ const App = () => {
 	};
 
 	return (
-		<View style={styles.container}>
+		<SafeAreaView style={styles.container}>
+			<View style={styles.signInContainer}>
+				<TouchableOpacity style={styles.button} onPress={onPress}>
+					<Text style={styles.buttonText}>Sign In</Text>
+				</TouchableOpacity>
+			</View>
+
 			<Text style={styles.h1}>Bumpt</Text>
 			<Text style={styles.h2}>Virtual business cards</Text>
+			
 			<TouchableOpacity style={styles.button} onPress={onPress}>
 				<Text style={styles.buttonText}>Sign Up with Google</Text>
 			</TouchableOpacity>
@@ -28,20 +35,21 @@ const App = () => {
 			<TouchableOpacity style={styles.button} onPress={onPress}>
 				<Text style={styles.buttonText}>Sign Up with Email</Text>
 			</TouchableOpacity>
+			
 			<Text style={styles.p}>By signing up, you agree to our Terms of Service</Text>
 			<Text style={styles.p}>and Privacy Policy.</Text>
-		</View>
+		</SafeAreaView>
 	);
 };
 
 const styles = StyleSheet.create({
 	container: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center'
+		backgroundColor: 'black',
+		flex: 1
 	},
 	h1: {
-		fontSize: 32
+		fontSize: 32,
+		color: '#0099ff'
 	},
 	h2: {
 		marginBottom: 32,
@@ -53,12 +61,12 @@ const styles = StyleSheet.create({
 	button: {
 		width: '90%',
 		marginBottom: 16,
-		padding: 12,
+		padding: 10,
 		borderRadius: 4,
 		backgroundColor: '#ffffff'
 	},
 	buttonText: {
-		fontSize: 16,
+		fontSize: 14,
 		color: '#333',
 		textAlign: 'center'
 	}
